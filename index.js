547b1a7c2c38b397683cdd173648e3b8cf6c6edc
jaydeep9924 <jaydeepatel9924@gmail.com>
2023-07-10 17:26:35 +0530

    update data

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const port = 8010;
 const app = express();
 
 app.use(express.urlencoded());
-const passport = require('passport')
+const passport = require('passport');
 
 // const db = require('./config/mongoose');
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 const url = `mongodb+srv://jaydeepatel:Jaydeep123@cluster0.bscpzyc.mongodb.net/Three_Level_Api?retryWrites=true&w=majority`;
 
 mongoose.connect(url,{
   useNewUrlParser: true,
   useUnifiedTopology: true 
 })
-    .then( () => {
-        console.log('Connected to database ')
-    })
-    .catch( (err) => {
-        console.error(`Error connecting to the database. \n${err}`);
-    })
+  .then( () => {
+    console.log('Connected to database ')
+  })
+  .catch( (err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  })
+
 
 const passport_jwt = require('./config/passport_jwt');
-
 const session = require('express-session');
 
 app.use(session({
