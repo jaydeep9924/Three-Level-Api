aa66a58af52711a4b99059d29b83d558e97e486c
jaydeep9924 <jaydeepatel9924@gmail.com>
2023-07-10 12:00:20 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const app = express();
 app.use(express.urlencoded());
 const passport = require('passport')
 
-const db = require('./config/mongoose');
+// const db = require('./config/mongoose');
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://jaydeepatel:Jaydeep123@cluster0.bscpzyc.mongodb.net/Three_Level_Api?retryWrites=true&w=majority`;
+
+mongoose.connect(url,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+})
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 const passport_jwt = require('./config/passport_jwt');
 
--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,7 @@ const passport_check = require('../config/passport_jwt');
 const adminCon = require('../controller/admin');
 const admin = require('../model/admindata');
 
-router.get('/getdata', adminCon.getdata);
-router.post('/data', adminCon.data);
-
-router.delete('/datadelete/:id', adminCon.deletedata);
-router.put('/updatedata/:id', adminCon.updatedata);
-router.patch('/patchdata/:id', adminCon.patchdata);
-
-router.post('/admindata', passport_check.authenticate('admin',{failureRedirect : '/loginpage'}),admin.imguploaded,adminCon.admindata);
+router.post('/admindata', admin.imguploaded,adminCon.admindata);
 router.get('/getadmindata',passport_check.authenticate('admin',{failureRedirect : '/loginpage'}), adminCon.getadmindata);
 router.delete('/deleteadmin/:id', passport_check.authenticate('admin',{failureRedirect : '/loginpage'}), adminCon.deleteadmin);
 
@@ -21,7 +14,7 @@ router.get('/loginpage', (req,res)=>{
   res.json({status : 400, msg : 'Login Required'})
 });
 
-router.post ('/login', adminCon.login);
+router.post ('/login',  adminCon.login);
 router.use('/faculty',require('../routes/v1/facultyRouter'));
 router.use('/student',require('../routes/v1/studentRouter'));
 
